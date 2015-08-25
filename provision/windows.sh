795f3776ad33b30f670091b0f2dafc32aad066ca
Marco Vito Moscaritolo <mavimo@gmail.com>
2015-08-25 17:45:50 +0200

    Added configuration to use windows machine as host.

--- provision/windows.sh
+++ provision/windows.sh
@@ -25,7 +25,7 @@ sudo apt-get update
 sudo apt-get install -y ansible
 
 # Setup Ansible for Local Use and Run
-cp /vagrant/ansible/inventories/dev /etc/ansible/hosts -f
+cp /vagrant/provision/inventories/dev /etc/ansible/hosts -f
 chmod 666 /etc/ansible/hosts
-cat /vagrant/ansible/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
-sudo ansible-playbook /vagrant/ansible/playbook.yml -e hostname=$1 --connection=local
+cat /vagrant/provision/files/authorized_keys >> /home/vagrant/.ssh/authorized_keys
+sudo ansible-playbook /vagrant/provision/playbook.yml -e hostname=$1 --connection=local
